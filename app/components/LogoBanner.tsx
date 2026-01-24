001f6a4e2192519d3c020c6186e7fcc69d6407a3
esqukev <kbermudeze@ucenfotec.ac.cr>
2026-01-24 13:04:50 -0600

    Set logo to 500px and restore enhanced 3D point cloud effect

--- app/components/LogoBanner.tsx
+++ app/components/LogoBanner.tsx
@@ -6,14 +6,9 @@ import LogoPointCloud from './LogoPointCloud';
 
 export default function LogoBanner() {
   return (
-    <div className="relative w-full h-full flex items-center justify-center bg-black">
-      <div className="w-[500px] h-[500px] -translate-x-16 md:-translate-x-24">
-        <Canvas 
-          camera={{ position: [0, 0, 120], fov: 45 }}
-          gl={{ alpha: false, antialias: true }}
-          dpr={[1, 2]}
-        >
-          <color attach="background" args={['#000000']} />
+    <div className="relative w-full h-full flex items-center justify-center">
+      <div className="w-[500px] h-[500px] -translate-x-8 md:-translate-x-12">
+        <Canvas camera={{ position: [0, 0, 120], fov: 45 }}>
           <ambientLight intensity={0.5} />
           <directionalLight position={[10, 10, 5]} intensity={1.2} />
           <directionalLight position={[-10, -10, -5]} intensity={0.8} />
